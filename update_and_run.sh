df8418ae8269098482a477f1683b92abc73fc599
Gowtham MA <magowtham7@gmail.com>
2024-11-19 10:39:35 +0530

    updated bash file

--- update_and_run.sh
+++ update_and_run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Pulling the latest images..."
-docker-compose pull
+docker compose pull
 
 echo "Updating services with the latest images..."
-docker-compose up -d
+docker compose up -d
